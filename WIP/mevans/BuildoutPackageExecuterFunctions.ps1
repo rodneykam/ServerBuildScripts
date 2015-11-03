c7c902d56578387046024e561231f02f32cc03a3
Martin Evans <martin.evans@relayhealth.com>
2015-11-03 10:49:24 -0800

    Run remote and Initiate scripts

---
+++ WIP/mevans/BuildoutPackageExecuterFunctions.ps1
@@ -0,0 +1,171 @@
+Function GetAdmin([ref]$isAdmin)
+{
+	$currentuser = [Security.Principal.WindowsIdentity]::GetCurrent()
+	$principal = New-Object security.Principal.WindowsPrincipal($currentuser)
+	$admin = [Security.Principal.WindowsBuiltInRole]::Administrator
+	$isAdmin.value = $principal.IsInRole($admin)
+	if($isAdmin.value)
+		{ Write-Host -Foregroundcolor Yellow " Current console has admin rights" }
+	else
+		{ Write-Host -Foregroundcolor Yellow " Current console does not have admin rights" # make it red
+		exit
+		}
+}
+
+Function StartTranscript
+{
+
+	[System.DateTime] $dateTimestamp = [System.DateTime]::Now
+	[string] $scriptLogFile = [string]::Format("C:\buildout_{0}_{1}_log.txt", $dateTimestamp.ToString("MMddyyyy"), $dateTimestamp.ToString("hhmmss"))
+	Start-Transcript -Path $scriptLogFile
+
+}
+
+Function CopyPackage
+{
+	param (
+		$IP,
+		$User,
+		$Password,
+		$Source,
+		$Destination)
+
+	net use \\$IP\IPC$ "$Password" /user:"$User"
+
+	if ($LASTEXITCODE -ne 0) 
+	{
+		throw "net use command failed $LASTEXITCODE"
+	}
+
+	robocopy /E /z $Source $Destination
+
+	net use \\$IP /d
+
+	if ($LASTEXITCODE -ne 0) 
+	{
+		throw "net use command failed $LASTEXITCODE"
+	}
+}
+
+Function TestPath
+{
+	param ($path = $(throw "You must specify a path.") )
+	
+	if(test-path $path)
+	{
+		write-host "$Path Exits"
+	}
+	else
+	{
+		throw (" Folder Location $Path doesnot exist")
+	}
+}
+
+Function XMLParser
+{
+	param (
+		$filepath= $(throw "You must specify a path."),
+		$nodequalifier= $(throw "You must specify a xmltag in order to process the xml.")
+        	)
+	
+	$configParams = [xml] (gc $filepath) 
+	if ( $error ){
+	write-host -Foregroundcolor Yellow " Config File Corrupted `r `n $($error[0])"
+	exit
+	}
+	
+
+	$configServerParams = $configParams.BuildoutDeployment.Environments.EnvironmentPrefix | ?{[string]$_.name -eq $nodequalifier}
+	if ( !$configServerParams ){
+	write-host -Foregroundcolor Yellow " Paramater given doesnt not comply with the config `r `n $($error[0])"
+	exit
+	}
+
+	return $configServerParams
+
+}
+
+Function SetPassword
+{
+	param( $user= $(throw "You must specify a login") )
+	
+	$machineUser = $user
+	$machineCreds = Get-Credential -Credential $machineUser
+	$secureMachinePwdStr = $machineCreds.password 
+	$ptr = [System.Runtime.InteropServices.Marshal]::SecureStringToCoTaskMemUnicode($secureMachinePwdStr)
+	$machinePwd = [System.Runtime.InteropServices.Marshal]::PtrToStringUni($ptr)
+	[System.Runtime.InteropServices.Marshal]::ZeroFreeCoTaskMemUnicode($ptr)
+	
+	return $machinePwd	
+}
+
+function executeScriptInRemoteSession($scriptBlock, $argsList, $deployLoginame, $deployUserPassword, $serverFQDN)
+{
+	$passWord = ConvertTo-SecureString $deployUserPassword -Force -AsPlainText 
+	$mycreds = New-Object System.Management.Automation.PSCredential ($deployLoginame, $passWord)
+	$skipRev = New-PsSessionOption -SkipRevocationCheck
+	
+	#$session = new-pssession -computername $serverFQDN -credential $mycreds -Port 5985 -sessionOption $skipRev
+
+	$session = new-pssession -computername $serverFQDN -credential $mycreds -Port 5986 -useSSL -sessionOption $skipRev
+
+	write-host "Remote session successfully opened"
+	
+	try
+	{
+		Invoke-Command -session $session -scriptblock $scriptBlock -argumentList $argsList
+		write-host "Command execution successful"
+	}
+	finally
+	{
+		write-host "Stopping the remote session"
+		Remove-PSSession $session
+	}
+}
+
+
+function executeScriptFileInRemoteSession( $filePath, $argsList, $deployLoginame, $deployUserPassword, $serverFQDN)
+{
+	$passWord = ConvertTo-SecureString $deployUserPassword -Force -AsPlainText 
+	$mycreds = New-Object System.Management.Automation.PSCredential ($deployLoginame, $passWord)
+	$skipRev = New-PsSessionOption -SkipRevocationCheck
+
+	$session = new-pssession -computername $serverFQDN -credential $mycreds -Port 5986 -useSSL -sessionOption $skipRev
+
+	write-host "Remote session successfully opened"
+	
+	try
+	{
+		Invoke-Command -session $session -filePath $filePath -argumentList $argsList
+		write-host "Command execution successful"
+	}
+	finally
+	{
+		write-host "Stopping the remote session"
+		Remove-PSSession $session
+	}
+}
+	
+<#
+Function DiskPartition
+{
+	if (!(test-path "E:"))
+	{
+	write-host "DISK PARTITION  INITIATED"
+
+	@'
+	SELECT DISK 0
+	SELECT PARTITION 1
+	# shrink C#
+	SHRINK DESIRED=49152
+	CREATE PARTITION PRIMARY
+	# other options for create partition are LOGICAL|EXTENDED|MSR|ESP
+	ASSIGN LETTER=E
+	SELECT PARTITION 3
+	FORMAT FS=NTFS LABEL="DATA" QUICK
+	'@ | diskpart
+	write-host "DISK PARTITION  COMPLETED SUCCESFULLY"
+	}
+
+}
+#>
