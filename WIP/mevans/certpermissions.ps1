ea6bbff58b0a1146d1ef2ea843f78d5d082463b9
Martin Evans <martin.evans@relayhealth.com>
2015-11-03 20:04:24 -0800

    Grant certificate access to account using invoke-expression rather than a bat file

---
+++ WIP/mevans/certpermissions.ps1
@@ -0,0 +1,43 @@
+
+<# Expected output is:
+Microsoft (R) WinHTTP Certificate Configuration Tool
+Copyright (C) Microsoft Corporation 2001.
+
+Matching certificate:
+CN=MEVANS-Z620.healinx.inside
+
+Granting private key access for account:
+    NT AUTHORITY\NETWORK SERVICE
+
+or:
+Microsoft (R) WinHTTP Certificate Configuration Tool
+Copyright (C) Microsoft Corporation 2001.
+
+Matching certificate:
+CN=MEVANS-Z620.healinx.inside
+
+
+Private key access has already been granted for account:
+    NT AUTHORITY\NETWORK SERVICE
+#>
+
+$certName="MEVANS-Z620.healinx.inside"
+$account="Network Service"
+$result=invoke-expression "C:\GitHub\ServerBuildScripts\WIP\mevans\winhttpcertcfg.exe -g -a `"$account`"  -c LOCAL_MACHINE\My -s $certName"
+
+if ($LastExitCode -ne 0) {
+	Write-host -ForegroundColor Red "Certificate permissions command failed to run"
+	exit
+}
+if (!($result -match $certName)) {
+	Write-host -ForegroundColor Red "Could not find the certificate $certName"
+	exit
+}
+if ($result -match "Private key access has already been granted for account:") {
+	Write-host -ForegroundColor Yellow "Certificate access permissions already granted for account $account on certificate $certName"
+}
+if ($result -match "Granting private key access for account:") {
+	Write-host -ForegroundColor Yellow "Granted certificate access permissions for account $account on certificate $certName"
+}
+# Show result text
+$result
