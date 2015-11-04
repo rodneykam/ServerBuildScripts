24d2396550baaf58468ee1f5ef275f9d73023301
Martin Evans <martin.evans@relayhealth.com>
2015-11-03 20:12:56 -0800

    Loop through the accounts and certificates. Error handling. Parsing output messages.

--- WIP/mevans/certpermissions.ps1
+++ WIP/mevans/certpermissions.ps1
@@ -21,23 +21,31 @@ Private key access has already been granted for account:
     NT AUTHORITY\NETWORK SERVICE
 #>
 
-$certName="MEVANS-Z620.healinx.inside"
-$account="Network Service"
-$result=invoke-expression "C:\GitHub\ServerBuildScripts\WIP\mevans\winhttpcertcfg.exe -g -a `"$account`"  -c LOCAL_MACHINE\My -s $certName"
+# Loop through the three certificates (EMR, HealthVault and VortexrToolsClientCert)
+foreach ($certName in "MEVANS-Z620.healinx.inside","MEVANS-Z620.na.corp.mckesson.com") {
+	Write-host -ForegroundColor Yellow "Processing certificate $certName"
+	
+	# Loop through the two accounts (Network Service and RelayServices)
+	foreach ($account in "Network Service","ek8t4xb") {
+		Write-host -ForegroundColor Yellow "Processing account $account on certificate $certName"
+		$result=invoke-expression "C:\GitHub\ServerBuildScripts\WIP\mevans\winhttpcertcfg.exe -g -a `"$account`"  -c LOCAL_MACHINE\My -s $certName"
 
-if ($LastExitCode -ne 0) {
-	Write-host -ForegroundColor Red "Certificate permissions command failed to run"
-	exit
+		# Error handling
+		if ($LastExitCode -ne 0) {
+			Write-host -ForegroundColor Red "Certificate permissions command failed to run"
+			exit
+		}
+		if (!($result -match $certName)) {
+			Write-host -ForegroundColor Red "Could not find the certificate $certName"
+			exit
+		}
+		if ($result -match "Private key access has already been granted for account:") {
+			Write-host -ForegroundColor Yellow "Certificate access permissions already granted for account $account on certificate $certName"
+		}
+		if ($result -match "Granting private key access for account:") {
+			Write-host -ForegroundColor Yellow "Granted certificate access permissions for account $account on certificate $certName"
+		}
+		# Show result text
+		$result
+	}
 }
-if (!($result -match $certName)) {
-	Write-host -ForegroundColor Red "Could not find the certificate $certName"
-	exit
-}
-if ($result -match "Private key access has already been granted for account:") {
-	Write-host -ForegroundColor Yellow "Certificate access permissions already granted for account $account on certificate $certName"
-}
-if ($result -match "Granting private key access for account:") {
-	Write-host -ForegroundColor Yellow "Granted certificate access permissions for account $account on certificate $certName"
-}
-# Show result text
-$result
