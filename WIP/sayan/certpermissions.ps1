2cd38d87ae43b833a1fb1b7499be0e51cbde2b59
Sayandip Bhattacharya <sayandip.bhattacharya@relayhealth.com>
2015-11-18 15:53:36 -0800

    refactoring

--- WIP/sayan/certpermissions.ps1
+++ WIP/sayan/certpermissions.ps1
@@ -46,51 +46,85 @@ param
 (
 	[Parameter(Mandatory=$true)] $EnvironmentConfig,
 	[Parameter(Mandatory=$true)] $MachineConfig
-	[Parameter(Mandatory=$true)] $BuildOutLog
-
 )
 
-Write-Log -ForegroundColor Green "`nStart of Certificate Permissions script`n"
+Write-host -ForegroundColor Green "`nStart of Certificate Permissions script`n"
 
-### START
-### Find the certificates and the winhttpcertcfg executable, per Tracy's registerAppCert.ps1 script
-### ....
+# Find the certificates and the winhttpcertcfg executable
 $emrsubject = "emr-prod.relayhealth.com"
 $hvaultsubject = "healthvault.relayhealth.com"
 $vortexsubject = "VortexrToolsClient"
 
-$account = [String]$MachineConfig.HVRelayServicesAccount
-# ...
-# END
+# Set folder and file locations
+$wincertdir = "E:\RegisterAppCert"
+$certConfig = "E:\RegisterAppCert\winhttpcertcfg.exe"  #executable that gets run in each batch file
 
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+write-host -foreground Green "`n Checking for existing RegisterAppCert folder and file section."
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+
+# Test for existence of the E:\RegisterAppCert folder and the winhttpcertcfg.exe file
+if (-Not (test-path $wincertdir)) 
+{
+	Write-Host -ForegroundColor Red "   The E:\RegisterAppCert folder does not exist."
+	Write-Host -ForegroundColor Red "   Terminating registerAppCert script. `n"
+	Break
+}
+elseif (-Not (test-path $certConfig)) 
+{
+	Write-Host -ForegroundColor Yellow "   The E:\RegisterAppCert folder exists, testing for winhttpcertcfg.exe"
+	Write-Host -ForegroundColor Red "   The file winhttpcertcfg.exe does not exist"
+	Write-Host -ForegroundColor Red "   Terminating registerAppCert script. `n"
+    Break
+}
+else
+{
+	Write-Host -ForegroundColor Yellow "   The E:\RegisterAppCert folder exists."
+	Write-Host -ForegroundColor Yellow "   The file winhttpcertcfg.exe exists"
+}
+
+$account = [String]$MachineConfig.HVRelayServicesAccount
 
 # Loop through the three certificates (EMR, HealthVault and VortexrToolsClientCert)
 foreach ($certName in $emrsubject,$hvaultsubject,$vortexsubject) {
-	Write-Log "- Processing certificate $certName" $BuildOutLog Info
+	Write-host -ForegroundColor Yellow "- Processing certificate $certName"
+	$certificate = gci cert:\LocalMachine\MY | where-object {$_.Subject -match "$certName"}
+
+	# Check for the existance of the certificate in the cert directory
+	if([string]::IsNullOrEmpty($certificate))
+	{
+		Write-Host -ForegroundColor Yellow "   The $certName certificate does not exist in the mmc or the DTD does not have the right value `n"
+		throw "The $certName certificate does not exist in the mmc or the DTD does not have the right value"
+	}
+	else
+	{
+		Write-Host -ForegroundColor Yellow "   Found the $certName certificate."
+	}
 	
 	# Loop through the two accounts (Network Service and the server-specific RelayServices accounts)
 	foreach ($account in "Network Service",$account) {
-		Write-Log "-- Processing account $account on certificate $certName"
-		$result=invoke-expression "E:\healthvault\\winhttpcertcfg.exe -g -a `"$account`"  -c LOCAL_MACHINE\My -s $certName"
+		Write-host -ForegroundColor Yellow "-- Processing account $account on certificate $certName"
+		$result=invoke-expression "E:\healthvault\winhttpcertcfg.exe -g -a `"$account`"  -c LOCAL_MACHINE\My -s $certName"
 
 		# Error handling
 		if ($LastExitCode -ne 0) {
-			Write-Log "Certificate permissions command failed to run" $BuildOutLog Error
+			Write-host -ForegroundColor Red "Certificate permissions command failed to run"
 			exit
 		}
 		if (!($result -match $certName)) {
-			Write-Log "Could not find the certificate $certName" $BuildOutLog Error
+			Write-host -ForegroundColor Red "Could not find the certificate $certName"
 			exit
 		}
 		if ($result -match "Private key access has already been granted for account:") {
-			Write-Log "Certificate access permissions already granted for account $account on certificate $certName" $BuildOutLog Warn
+			Write-host -ForegroundColor Yellow "Certificate access permissions already granted for account $account on certificate $certName"
 		}
 		if ($result -match "Granting private key access for account:") {
-			Write-Log "Granted certificate access permissions for account $account on certificate $certName" $BuildOutLog Warn
+			Write-host -ForegroundColor Yellow "Granted certificate access permissions for account $account on certificate $certName"
 		}
 		# Show result text
 		$result
 	}
 }
 
-Write-Log "`nEnd of Certificate Permissions script`n" $BuildOutLog Info
+Write-host -ForegroundColor Green "`nEnd of Certificate Permissions script`n"
--- WIP/sayan/permtest.ps1
+++ WIP/sayan/permtest.ps1
@@ -1,104 +1,24 @@
-# 4:43 PM 10/30/2015
-# .SYNOPSIS
-        
-# This script delets URL acl's for several web sites.
-# It then adds them back in with the proper permissions.
-        
-# .DESCRIPTION
-# 
-# .EXAMPLE
-        
-# .\permtest.ps1 $EnvironmentConfig $MachineConfig        
-
-#######################################################################################
-# Script begins
-# Here we set the Parameters for this script.
-
 param
 (
 [Parameter(Mandatory=$true)] $EnvironmentConfig,
 [Parameter(Mandatory=$true)] $MachineConfig
-[Parameter(Mandatory=$true)] $BuildOutLog
 )
 
-Write-Log "$ScriptName `t$Date `n " $BuildOutLog Info
-
-# Here we delete the URL acl's
-# We capture the output in the $Output variable and write that to the logfile we created above.
-
-$Output = netsh http Delete urlacl url=http://+:8888/patient/DrugBenefitRxHistoryService
-Write-Log "Delete urlacl url=http://+:8888/patient/DrugBenefitRxHistoryService" $BuildOutLog Info
-Write-Log $Output $BuildOutLog Info
-
-
-$Output = netsh http Delete urlacl url=http://+:8080/EScriptIntegration
-Write-Log "Delete urlacl url=http://+:8080/EScriptIntegration" $BuildOutLog Info
-Write-Log $Output $BuildOutLog Info
-
-$Output = netsh http Delete urlacl url=http://+:4762/PayorEligibilityService
-Write-Log "Delete urlacl url=http://+:4762/PayorEligibilityService" $BuildOutLog Info
-Write-Log $Output $BuildOutLog Info
-
-$Output = netsh http Delete urlacl url=http://+:2201/relayhealth/service/surescripts/prescriber
-Write-Log "Delete urlacl url=http://+:2201/relayhealth/service/surescripts/prescriber" $BuildOutLog Info
-Write-Log $Output $BuildOutLog Info
-
-$Output = netsh http Delete urlacl url=http://+:2201/relayhealth/service/rxhub/prescriber
-Write-Log "Delete urlacl url=http://+:2201/relayhealth/service/rxhub/prescriber" $BuildOutLog Info
-Write-Log $Output$BuildOutLog Info
-
-$Output = netsh http Delete urlacl url=http://localhost:8888/patient/DrugBenefitRxHistoryService
-Write-Log "Delete urlacl url=http://localhost:8888/patient/DrugBenefitRxHistoryService" $BuildOutLog Info
-Write-Log $Output $BuildOutLog Info
-
-$Output = netsh http Delete urlacl url=http://localhost:4762/PayorEligibilityService
-Write-Log "Delete urlacl url=http://localhost:4762/PayorEligibilityService" $BuildOutLog Info
-Write-Log $Output $BuildOutLog Info
-
-$Output = netsh http Delete urlacl url=http://+:4240/relayhealth/simulator/faxServer
-Write-Log "http Delete urlacl url=http://+:4240/relayhealth/simulator/faxServer" $BuildOutLog Info
-Write-Log $Output $BuildOutLog Info
-
-# Here we get the service account from the machine config 
-
 $perm = $MachineConfig.RelayServicesAccount
-Write-Log "the service account from the machine config" $BuildOutLog Info
-Write-Log "MachineConfig.RelayServicesAccount is $perm" $BuildOutLog Info
+$urls = @(
+"http://+:8888/patient/DrugBenefitRxHistoryService/",
+"http://+:8080/EScriptIntegration/",
+"http://+:4762/PayorEligibilityService/",
+"http://+:2201/relayhealth/service/surescripts/prescriber/",
+"http://+:2201/relayhealth/service/rxhub/prescriber/",
+"http://localhost:8888/patient/DrugBenefitRxHistoryService/",
+"http://localhost:4762/PayorEligibilityService/",
+"http://+:4240/relayhealth/simulator/faxServer/"
+)
 
-# Here we add the url's with the correct user 
+foreach ($url in $urls) {
+	netsh http Delete urlacl url=$url
+	netsh http add urlacl url="$url" user=$perm
+}
 
-$Output = netsh http add urlacl url=http://+:8888/patient/DrugBenefitRxHistoryService/ user=$perm
-Write-Log "add urlacl url=http://+:8888/patient/DrugBenefitRxHistoryService/ user=$perm" $BuildOutLog Info
-Write-Log $Output $BuildOutLog Info
 
-$Output = netsh http add urlacl url=http://+:8080/EScriptIntegration/ user= $perm
-Write-Log "add urlacl url=http://+:8080/EScriptIntegration/ user= $perm" $BuildOutLog Info
-Write-Log $Output $BuildOutLog Info
-
-$Output = netsh http add urlacl url=http://+:4762/PayorEligibilityService/ user= $perm
-Write-Log "add urlacl url=http://+:4762/PayorEligibilityService/ user= $perm" $BuildOutLog Info
-Write-Log $Output $BuildOutLog Info
-
-$Output = netsh http add urlacl url=http://+:2201/relayhealth/service/surescripts/prescriber/ user= $perm
-Write-Log "add urlacl url=http://+:2201/relayhealth/service/surescripts/prescriber/ user= $perm" $BuildOutLog Info
-Write-Log $Output $BuildOutLog Info
-
-$Output = netsh http add urlacl url=http://+:2201/relayhealth/service/rxhub/prescriber/ user= $perm
-Write-Log "add urlacl url=http://+:2201/relayhealth/service/rxhub/prescriber/ user= $perm" $BuildOutLog Info
-Write-Log $Output $BuildOutLog Info
-
-$Output = netsh http add urlacl url=http://localhost:8888/patient/DrugBenefitRxHistoryService/ user= $perm
-Write-Log "add urlacl url=http://localhost:8888/patient/DrugBenefitRxHistoryService/ user= $perm" $BuildOutLog Info
-Write-Log $Output $BuildOutLog Info
-
-$Output = netsh http add urlacl url=http://localhost:4762/PayorEligibilityService/ user=$perm 
-Write-Log "add urlacl url=http://localhost:4762/PayorEligibilityService/ user=$perm" $BuildOutLog Info
-Write-Log $Output $BuildOutLog Info
-
-$Output = netsh http add urlacl url=http://+:4240/relayhealth/simulator/faxServer/ user= $perm
-Write-Log "add urlacl url=http://+:4240/relayhealth/simulator/faxServer/ user= $perm" $BuildOutLog Info
-Write-Log $Output $BuildOutLog Info
-
-$Date = get-date -f "MM-dd-yyyy hh-mm-ss"
-
-Write-Log "End of $ScriptName `t$Date `n " $BuildOutLog Info
